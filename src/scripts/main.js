7debf41cbb909ca5c04ab49b97b07c9f2fc57e04
LukasVedral <lukas.vedral.023@pslib.cz>
2025-05-29 14:05:52 +0200

    swiper init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 import '../styles/style.css';
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation} from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+modules: [Navigation],
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
